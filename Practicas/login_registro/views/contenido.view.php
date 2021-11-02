4c70dd69d977ca9a951742f9f7307129bde3e1e0
AlvaroCasa <aramirezm098@gmail.com>
2021-11-02 14:37:10 +0100

    Fin de la primera practica, comienzo con la segunda

---
+++ Practicas/login_registro/views/contenido.view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contenido</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">    
+<link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div class="contenedor">
+        <h1 class="titulo">Contenido del sitio</h1>
+        <a href="cerrar.php">Cerrar Sesion</a>
+<hr class="border">
+    <div class="contenido">
+        <article >
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit sunt minus voluptas omnis tenetur placeat iste? Incidunt fugit fugiat debitis soluta numquam minus sunt eligendi, asperiores dolores nemo earum labore?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta exercitationem sequi, quaerat sed error itaque ea, totam aliquid est consequuntur minima molestias qui ad odio placeat quod porro quisquam nostrum.</p>
+        </article>
+    </div>
+
+    </div>
+
+</body>
+</html>
